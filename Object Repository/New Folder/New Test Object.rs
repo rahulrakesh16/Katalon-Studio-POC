510eb86244904931c7a92cbed5c5073c546922ce
rakesh.sm <rakesh.sm@host.docker.internal>
2022-08-11 12:11:34 +0530

    testcommit

---
+++ Object Repository/New Folder/New Test Object.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>d0d1bbd3-5186-4ff8-b599-6ecfed089228</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
